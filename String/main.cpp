bd8d24cdee6c57c78a6280c619733a8ffcdf7035
Petro_qj70@top-academy.ru <Petro_qj70@top-academy.ru>
2022-11-04 20:14:00 +0300

    Header_inicialition_done

--- String/main.cpp
+++ String/main.cpp
@@ -28,33 +28,28 @@ public:
 	}
 
 	// Constraction
-	explicit String(size_t size = 80)
+	explicit String(size_t size = 80):size(size),str(new char[size]{})
 	{
-		this->size = size;
-		this->str = new char[size] {};
+		//this->size = size;
+		//this->str = new char[size] {};
 		cout << "DefConstructor:\t" << this << endl;
 	}
-	String(const char str[])
+	String(const char str[]):size(strlen(str)+1),str(new char[size]{})
 	{
-		this->size = strlen(str) + 1;
-		this->str = new char[size] {};
 		for (int i = 0; i < size; i++)this->str[i] = str[i];
 		cout << "Constructor:\t" << this << endl;
 	}
-	String(const String& other)
+	String(const String& other):size(other.size),str(new char[size]{})
 	{
-		//Deep copy
-		this->size = other.size;
-		this->str = new char[size] {};
 		for (int i = 0; i < size; i++)
 			this->str[i] = other.str[i];
 		cout << "CopyConstructor:" << this << endl;
 	}
-	String(String&& other)
+	String(String&& other):size(other.size),str(other.str)
 	{
 		//Shallow copy:
-		this->size = other.size;
-		this->str = other.str;
+		/*this->size = other.size;
+		this->str = other.str;*/
 		other.size = 0;
 		other.str = nullptr;
 		cout << "MoveConstructor:" << this << endl;
@@ -125,6 +120,8 @@ String operator+ (const String& left, const String& right)
 
 //#define CONSTRUCTORS_CHECK
 #define OPERATORS_PLUS_CHECK
+//#define WAYS_TO_CALL_CONSTRACTORS
+
 void main()
 {
 	setlocale(LC_ALL, "");
@@ -149,13 +146,23 @@ void main()
 #ifdef OPERATORS_PLUS_CHECK
 	String str1 = "Hello";
 	String str2 = "World";
-	/*String str3;
+	String str3;
 	str3 = str1 + " " + str2;
 	cout << str3 << endl;
 	str3 = str2;
-	cout << str3 << endl;*/
+	cout << str3 << endl;
 	str1 += str2;
 	cout << str1 << endl;
 #endif // OPERATORS_PLUS_CHECK
+#ifdef WAYS_TO_CALL_CONSTRACTORS
+	String str1;   //DefaultConstructor
+	String str2(5); //Single-argument
+	String str3 = "Hello"; //Single-argument type 'const char'
+	string str4(); //это выражение не создает обьект,а просто обьявляет функцию str4
+	// которая не чего не принимает, и возвращает значение типа 'String'
+	String str5{};  //явный вызов консруктора по умолчанию
+	String str6{ 7 };
+	String str7{ " Hello " };
+#endif // WAYS_TO_CALL_CONSTRACTORS
 
 }
