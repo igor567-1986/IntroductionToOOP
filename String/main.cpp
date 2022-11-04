864ed34cad039377e44856a4a877ef3a33efe5fc
Petro_qj70@top-academy.ru <Petro_qj70@top-academy.ru>
2022-11-04 20:50:32 +0300

    Dlegation_done

--- String/main.cpp
+++ String/main.cpp
@@ -34,17 +34,12 @@ public:
 		//this->str = new char[size] {};
 		cout << "DefConstructor:\t" << this << endl;
 	}
-	String(const char str[]):size(strlen(str)+1),str(new char[size]{})
+	String(const char str[]):String(strlen(str)+1)
 	{
 		for (int i = 0; i < size; i++)this->str[i] = str[i];
 		cout << "Constructor:\t" << this << endl;
 	}
-	String(const String& other):size(other.size),str(new char[size]{})
-	{
-		for (int i = 0; i < size; i++)
-			this->str[i] = other.str[i];
-		cout << "CopyConstructor:" << this << endl;
-	}
+	String(const String& other):String(other.str){	cout << "CopyConstructor:" << this << endl;	}
 	String(String&& other):size(other.size),str(other.str)
 	{
 		//Shallow copy:
